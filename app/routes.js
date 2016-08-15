bec81d9c7d069d281e53e0ff7584ba58029b4af8
Stuart Hiller <hiller.stuart@gmail.com>
2016-08-15 15:03:47 -0700

    edit distance function working

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,8 @@
+var path 		 = require('path');
+// var app 		 = require('express');
+
 module.exports = function(app) {
 	app.get('*', function(req, res) {
-		res.sendFile('./public/index.html')
+		res.sendFile(path.join(__dirname, '../public/index.html'))
 	});
 };
--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -1,5 +1,49 @@
-angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
+angular.module('NerdCtrl', []).controller('NerdController', ['$scope', function($scope) {
 
     $scope.tagline = 'Nothing beats a pocket protector!';
+    $scope.list = [];
+    console.log($scope.list);
+    $scope.text = '';
+    	getEditDistance = function(a, b){
+  			if(a.length == 0) return b.length; 
+  			if(b.length == 0) return a.length; 
 
-});
+  			var matrix = [];
+
+			  // increment along the first column of each row
+			  var i;
+			  for(i = 0; i <= b.length; i++){
+			    matrix[i] = [i];
+			  }
+
+			  // increment each column in the first row
+			  var j;
+			  for(j = 0; j <= a.length; j++){
+			    matrix[0][j] = j;
+			  }
+
+			  // Fill in the rest of the matrix
+			  for(i = 1; i <= b.length; i++){
+			    for(j = 1; j <= a.length; j++){
+			      if(b.charAt(i-1) == a.charAt(j-1)){
+			        matrix[i][j] = matrix[i-1][j-1];
+			      } else {
+			        matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
+			                                Math.min(matrix[i][j-1] + 1, // insertion
+			                                         matrix[i-1][j] + 1)); // deletion
+			      }
+			    }
+			  }
+			  return matrix[b.length][a.length];
+			};
+
+    $scope.submit = function() {
+      // event.prev entDefault();
+      if ($scope.text) {
+        $scope.list.push(this.text);
+        console.log($scope.text);
+        console.log(getEditDistance(this.text, "car"));
+    	}
+    };
+
+}]);
